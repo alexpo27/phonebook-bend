69ba43a85fc65279f81106c12006e37b3993203c
Alex P <alexandru.penes@idtotal.com>
2019-09-24 11:46:40 +0300

    removed build from gitignore

---
+++ build/precache-manifest.028c76d41cc580107f271fca1e2a9665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c251cbaaf1879395922e54ba8aa78e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5a0bf59e760a10c8ab6",
+    "url": "/static/css/main.7f7cbd33.chunk.css"
+  },
+  {
+    "revision": "e531ad2ce5ea044f30ae",
+    "url": "/static/js/2.19635263.chunk.js"
+  },
+  {
+    "revision": "c5a0bf59e760a10c8ab6",
+    "url": "/static/js/main.c804ddb9.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
